597971c60bb6e50609aa85ef83ac425d8045bda4
azaek <aloxel@gmail.com>
2022-01-22 00:38:03 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -189,7 +189,7 @@ export default function Home() {
     };
 
     const shortenAddress = (str) => {
-        return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+        return str?.substring(0, 6) + "..." + str?.substring(str.length - 4);
     };
 
     useEffect(() => {
